365f77054a21755861913a2cd5efbb671bc0d2d6
noedaka <karobka24@gmail.com>
2025-09-29 18:26:00 +0300

    fix: db initial error

--- internal/config/db/config.go
+++ internal/config/db/config.go
@@ -3,18 +3,18 @@ package dbconfig
 import "database/sql"
 
 func InitDB(db *sql.DB) error {
-    tx, err := db.Begin()
-    if err != nil {
-        return err
-    }
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
 
-    defer func() {
-        if err != nil {
-            tx.Rollback()
-        }
-    }()
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		}
+	}()
 
-    _, err = tx.Exec(`
+	_, err = tx.Exec(`
         CREATE TABLE IF NOT EXISTS users (
             id BIGSERIAL PRIMARY KEY,
             current REAL DEFAULT 0,
@@ -39,32 +39,32 @@ func InitDB(db *sql.DB) error {
         CREATE TABLE IF NOT EXISTS withdrawals (
             id BIGSERIAL PRIMARY KEY,
             user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-            order_number VARCHAR(255) NOT NULL UNIQUE,
-            sum REAL NOT NULL CHECK (sum > 0),
+            number VARCHAR(255) NOT NULL UNIQUE,
+            sum REAL NOT NULL CHECK (sum >= 0),
             processed_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
         );
     `)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    if err = tx.Commit(); err != nil {
-        return err
-    }
+	if err = tx.Commit(); err != nil {
+		return err
+	}
 
-    indexes := []string{
+	indexes := []string{
 		"CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_orders_user_id ON orders(user_id);",
 		"CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_orders_status ON orders(status);",
 		"CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_withdrawals_user_id ON withdrawals(user_id);",
-    }
+	}
 
-    for _, indexSQL := range indexes {
-        _, err = db.Exec(indexSQL)
-        if err != nil {
-            return err
-        }
-    }
+	for _, indexSQL := range indexes {
+		_, err = db.Exec(indexSQL)
+		if err != nil {
+			return err
+		}
+	}
 
-    return nil
+	return nil
 }
